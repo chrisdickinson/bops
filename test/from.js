3f4fb2991a446eb0dac10a124dbcd757b542864e
Chris Dickinson <christopher.s.dickinson@gmail.com>
2013-05-03 22:36:26 -0700

    add tests for "to"

--- test/from.js
+++ test/from.js
@@ -1,6 +1,17 @@
 var test = require('tape')
   , binary = require('../index')
 
+test('from array works', function(assert) {
+  var arr = [1, 2, 3]
+    , buf = binary.from(arr)
+
+  assert.equal(buf.length, arr.length)
+  for(var i = 0, len = arr.length; i < len; ++i) {
+    assert.equal(binary.readUInt8(buf, i), arr[i])
+  }
+  assert.end()
+})
+
 test('from utf8 works as expected', function(assert) {
   var buf = binary.from('ƒello 淾淾淾 hello world 淾淾 yep ƒuu 淾', 'utf8')
     , expect
@@ -34,20 +45,6 @@ test('from base64 works as expected', function(assert) {
     , expect
 
   expect = [104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 198, 146, 0, 10, 8, 9]
-
-  assert.equal(buf.length, expect.length)
-  for(var i = 0, len = buf.length; i < len; ++i) {
-    assert.equal(binary.readUInt8(buf, i), expect[i])
-  }
-
-  assert.end()
-})
-
-test('from binary works as expected', function(assert) {
-  var buf = binary.from('ƒello 淾淾淾 hello world 淾淾 yep ƒuu 淾', 'binary')
-    , expect
-
-  expect = [146,101,108,108,111,32,254,254,254,32,104,101,108,108,111,32,119,111,114,108,100,32,254,254,32,121,101,112,32,146,117,117,32,254]
 
   assert.equal(buf.length, expect.length)
   for(var i = 0, len = buf.length; i < len; ++i) {
