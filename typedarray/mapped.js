52636701a8199b9b251edc29ec4f293776b91eac
Chris Dickinson <christopher.s.dickinson@gmail.com>
2013-05-04 01:11:02 -0700

    fix issue with using weakmap

--- typedarray/mapped.js
+++ typedarray/mapped.js
@@ -5,7 +5,7 @@ module.exports = proto = {}
 
 map = typeof WeakMap === 'undefined' ? null : new WeakMap
 
-proto.get = map ? no_weakmap_get : get
+proto.get = !map ? no_weakmap_get : get
 
 function no_weakmap_get(target) {
   return new DataView(target.buffer, 0)
