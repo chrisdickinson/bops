9720a5241ea7d1394d409a04a4374ffb4af3f1c9
Chris Dickinson <christopher.s.dickinson@gmail.com>
2013-05-05 16:58:39 -0700

    fix bops.copy

--- typedarray/copy.js
+++ typedarray/copy.js
@@ -6,6 +6,23 @@ function copy(source, target, target_start, source_start, source_end) {
   target_start = arguments.length < 3 ? 0 : target_start
   source_start = arguments.length < 4 ? 0 : source_start
   source_end = arguments.length < 5 ? source.length : source_end
+
+  if(source_end === source_start) {
+    return
+  }
+
+  if(target.length === 0 || source.length === 0) {
+    return
+  }
+
+  if(source_end > source.length) {
+    source_end = source.length
+  }
+
+  if(target.length - target_start < source_end - source_start) {
+    source_end = target.length - target_start + start
+  }
+
   if(source.buffer !== target.buffer) {
     return fast_copy(source, target, target_start, source_start, source_end)
   }
@@ -13,11 +30,13 @@ function copy(source, target, target_start, source_start, source_end) {
 }
 
 function fast_copy(source, target, target_start, source_start, source_end) {
-  for(var i = target_start, len = source_end - source_start + target_start, j = source_start;
+  var len = (source_end - source_start) + target_start
+
+  for(var i = target_start, j = source_start;
       i < len;
       ++i,
       ++j) {
-    target[j] = source[i]
+    target[i] = source[j]
   }
 }
 
