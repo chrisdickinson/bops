4df4fc92884bd3bd8816b638a8ae5589726f22d8
Matteo Collina <matteo.collina@gmail.com>
2013-09-19 11:54:13 +0200

    Do not use bind in read and write for better performance.

--- write.js
+++ write.js
@@ -2,16 +2,19 @@ var Buffer = require('buffer').Buffer
 
 var proto = {}
   , rex = /write.+/
-  , fn
+  , buildFn
 
-fn = function() {
+buildFn = function(key) {
+  var code = '' +
+    'return buf.' + key + '(' + ['a', 'b', 'c'].join(',' ) + ')'
 
+  return new Function(['buf', 'a', 'b', 'c'], code)
 }
 
 module.exports = proto
 
 for(var key in Buffer.prototype) {
   if(rex.test(key)) {
-    proto[key] = fn.call.bind(Buffer.prototype[key])
+    proto[key] = buildFn(key)
   }
 }
