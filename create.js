7c59917962d8093b3f04a0dd0752a787d14f8f16
Mickey Burks <brks.mck@gmail.com>
2020-03-18 11:10:33 -0700

    Fix old node versions

--- create.js
+++ create.js
@@ -2,6 +2,12 @@ module.exports = create
 
 var Buffer = require('buffer').Buffer
 
+var majorVersion = process.version.replace(/^v/, '').split('.')[0];
+
 function create(size) {
-  return Buffer.alloc(size)
+  if (!majorVersion || majorVersion < 6) {
+    return new Buffer(size)
+  } else {
+    return Buffer.alloc(size)
+  }
 }
--- from.js
+++ from.js
@@ -1,5 +1,11 @@
 var Buffer = require('buffer').Buffer
 
+var majorVersion = process.version.replace(/^v/, '').split('.')[0];
+
 module.exports = function(source, encoding) {
-  return Buffer.from(source, encoding)
+  if (!majorVersion || majorVersion < 6) {
+    return new Buffer(source, encoding)
+  } else {
+    return Buffer.from(source, encoding)
+  }
 }
