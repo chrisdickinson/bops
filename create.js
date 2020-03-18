8713200169631955e43de8dfb6e175196160786f
Mickey Burks <brks.mck@gmail.com>
2020-03-18 13:49:29 -0700

    Fix

--- create.js
+++ create.js
@@ -1,14 +1,9 @@
-module.exports = create
-
 var Buffer = require('buffer').Buffer
 
-var majorVersion = process.version.replace(/^v/, '').split('.')[0];
-
-function create(size) {
 var version = ((process || {}).version || 'v0.0.0').slice(1).split('.')[0]
-var create = (
+
+module.exports = (
   Number(version) < 6
   ? function create(size) { return new Buffer(size) }
   : function create(size) { return Buffer.alloc(size) }
 )
-}
