c691d2d6d4caf272b9f2bb120ae32b5dbfef9f15
Mickey Burks <brks.mck@gmail.com>
2020-03-18 13:47:09 -0700

    Update create.js
    Co-Authored-By: Chris Dickinson <christopher.s.dickinson@gmail.com>

--- create.js
+++ create.js
@@ -5,9 +5,10 @@ var Buffer = require('buffer').Buffer
 var majorVersion = process.version.replace(/^v/, '').split('.')[0];
 
 function create(size) {
-  if (!majorVersion || majorVersion < 6) {
-    return new Buffer(size)
-  } else {
-    return Buffer.alloc(size)
-  }
+var version = ((process || {}).version || 'v0.0.0').slice(1).split('.')[0]
+var create = (
+  Number(version) < 6
+  ? function create(size) { return new Buffer(size) }
+  : function create(size) { return Buffer.alloc(size) }
+)
 }
